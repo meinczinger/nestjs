9637ddadf40889e3047a302df509879541e247c7
meinczinger <meinczinger@gmail.com>
2021-11-02 10:59:46 +0100

    section 3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // If nestjs encounters any validation decorators, it will know to execute validaiton pipes
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
