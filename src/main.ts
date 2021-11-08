c3c3c7db8b42e048ff8801b90297a1d6dc652d55
meinczinger <meinczinger@gmail.com>
2021-11-08 09:15:30 +0100

    logging added

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
 
   // If nestjs encounters any validation decorators, it will know to execute validaiton pipes
@@ -11,6 +14,10 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(3000);
+  const port = 3000;
+
+  await app.listen(port);
+
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
